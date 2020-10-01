07208bc1596c325a6bc2baa388b6823dbd8b07b2
guillem <guimarca@gmail.com>
2020-10-01 11:32:02 +0200

    feat(orders): create orders with products

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,61 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse, path
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from shop.models import Product, Order, OrderLine
+
+
+@admin.register(Product)
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "price", "created", "modified", "add_to_cart")
+    list_display_links = ("id",)
+    search_fields = ("name",)
+
+    def get_urls(self):
+        urls = super().get_urls()
+        custom_urls = [
+            path(
+                "add_to_cart/<path:product_id>",
+                self.admin_site.admin_view(self.add_product_to_cart)
+            )
+        ]
+        return urls + custom_urls
+
+    @staticmethod
+    def add_to_cart(item):
+        return mark_safe(f"<a href='add_to_cart/{item.id}'>Add to cart</a>")
+
+    @staticmethod
+    def add_product_to_cart(request, product_id):
+        product = get_object_or_404(Product, id=product_id)
+        order = Order.objects.filter(customer=request.user, shipped=False).order_by("-modified").first()
+        if order:
+            try:
+                order_line = OrderLine.objects.get(order=order, product=product)
+                order_line.quantity += 1
+                order_line.save()
+                messages.success(request, "Product already in the cart, one more unit was added")
+            except Exception:
+                OrderLine.objects.create(order=order, product=product)
+                messages.success(request, "Product added to your cart")
+        else:
+            order = Order.objects.create(customer=request.user)
+            OrderLine.objects.create(order=order, product=product)
+            messages.success(request, "Product added to your cart")
+        return redirect(reverse("admin:shop_order_change", args=(order.id,)))
+
+
+class OrderLineInLine(admin.TabularInline):
+    model = OrderLine
+    readonly_fields = ("total_amount",)
+
+
+@admin.register(Order)
+class ServerAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "shipped", "created", "modified")
+    list_display_links = ("id",)
+    list_filter = ("shipped",)
+    search_fields = ("name", "customer__username", "customer__first_name", "customer__lastname",)
+    inlines = (OrderLineInLine, )
+    change_form_template = "order_changeform.html"
--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,39 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+from model_utils.models import TimeStampedModel
+
+
+class Product(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=200, blank=False, null=False)
+    price = models.DecimalField(max_digits=12, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.name} ({self.price})"
+
+
+class Order(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    customer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="orders",
+    )
+    shipped = models.BooleanField(default=False)
+
+    @property
+    def total_amount(self):
+        return sum(pr.total_amount() for pr in list(self.order_products.all()))
+
+
+class OrderLine(TimeStampedModel):
+    product = models.ForeignKey(
+        Product, on_delete=models.CASCADE, related_name="order_lines",
+    )
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE, related_name="order_products",
+    )
+    quantity = models.PositiveSmallIntegerField(default=1)
+
+    def total_amount(self):
+        return self.quantity * self.product.price
